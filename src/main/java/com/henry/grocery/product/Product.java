d8b8c3a612b591b2e9305c72513741cd4cef9bbb
ahmedmohammed <ahmed.mohammed@underwriteme.co.uk>
2020-01-11 16:44:38 +0000

    equals and hashcode for product

--- src/main/java/com/henry/grocery/product/Product.java
+++ src/main/java/com/henry/grocery/product/Product.java
@@ -1,5 +1,7 @@
 package com.henry.grocery.product;
 
+import java.util.Objects;
+
 public class Product {
 
     private final String name;
@@ -22,5 +24,18 @@ public class Product {
 
     public double getCost() {
         return cost;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return name.equals(product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
